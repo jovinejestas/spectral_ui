5af4561cdad2dbd31377e48686fb8cbdecb84516
jovinejestas <jvjestas@gmail.com>
2022-06-18 09:41:34 +0300

    Added some stuffz

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export * from './components';
+
+//npm init
+//npm install react typescript @types/react --save-dev
+//npx tsc --init
+//npm install rollup @rollup/plugin-node-resolve @rollup/plugin-typescript @rollup/plugin-commonjs rollup-plugin-dts --save-dev
